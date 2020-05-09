35cae1221472b11f282120748589cd064acf9577
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2020-05-09 15:56:44 +0900

    Fix

--- src/main/java/com/example/demo/controller/CallbackController.java
+++ src/main/java/com/example/demo/controller/CallbackController.java
@@ -11,7 +11,7 @@ public class CallbackController {
 
     @GetMapping("/callback")
     public RedirectView callback(HttpServletResponse response) throws InterruptedException {
-        Thread.sleep(1000 * 100);
+        Thread.sleep(1000 * 3);
         System.out.println("You reached callback url :)");
         System.out.println("Go back to http://localhost:3000 again");
         return new RedirectView("http://localhost:3000");
