2ac18f4a218a188efa7e305a5cbcbecbbb2f804a
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2020-05-09 15:54:07 +0900

    Test Callback

---
+++ src/main/java/com/example/demo/controller/CallbackController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class CallbackController {
+
+    @GetMapping("/callback")
+    public RedirectView callback(HttpServletResponse response) throws InterruptedException {
+        Thread.sleep(1000 * 100);
+        System.out.println("You reached callback url :)");
+        System.out.println("Go back to http://localhost:3000 again");
+        return new RedirectView("http://localhost:3000");
+    }
+}
